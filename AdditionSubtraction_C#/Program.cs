c23a8734e3c16bacbc6fdfed093d12e646e2b1cb
«Vitaliy» <955550@list.ru>
2022-08-29 15:48:29 +0300

    Оригинал

--- AdditionSubtraction_C#/Program.cs
+++ AdditionSubtraction_C#/Program.cs
@@ -1,26 +1,26 @@
 ﻿// Сумма двух чисел, введенных пользователем. Подходит только для целых чисел (переменная int)
 
-//Console.WriteLine("Введите первое число");
-//string s = Console.ReadLine();
-//int a = Convert.ToInt32(s);
-//Console.WriteLine("Введите второе число");
-//string s2 = Console.ReadLine();
-//int b = Convert.ToInt32(s2);
-//int c = a + b;
-//Console.WriteLine("Сумма чисел");
-//Console.WriteLine(c);
+Console.WriteLine("Введите первое число");
+string s = Console.ReadLine();
+int a = Convert.ToInt32(s);
+Console.WriteLine("Введите второе число");
+string s2 = Console.ReadLine();
+int b = Convert.ToInt32(s2);
+int c = a + b;
+Console.WriteLine("Сумма чисел");
+Console.WriteLine(c);
 
 // Произведение двух чисел, введенных пользователем. Подходит для всех чисел (переменная doudle)
 
-//Console.WriteLine("Введите первое число");
-//string s = Console.ReadLine();
-//double a = Convert.ToDouble(s);
-//Console.WriteLine("Введите второе число");
-//string s2 = Console.ReadLine();
-//double b = Convert.ToDouble(s2);
-//double c = a * b;
-//Console.WriteLine("Произведение чисел");
-//Console.WriteLine(c);
+Console.WriteLine("Введите первое число");
+string s = Console.ReadLine();
+double a = Convert.ToDouble(s);
+Console.WriteLine("Введите второе число");
+string s2 = Console.ReadLine();
+double b = Convert.ToDouble(s2);
+double c = a * b;
+Console.WriteLine("Произведение чисел");
+Console.WriteLine(c);
 
 // Частное двух чисел, введенных пользователем. Подходит для всех чисел (переменная float)
 
